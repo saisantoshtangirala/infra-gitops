3dcced271a9ea56fda657f6957ae43ca2a24bcb0
t.sai <t.sai@aligntech.com>
2025-04-20 00:05:55 +0530

    fixed vpc and sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,43 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name = "gitops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+}
+
 data "aws_secretsmanager_secret_version" "db" {
   secret_id = "db_credentials"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "ecs-web-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-web-sg"
+  }
 }
 
 resource "aws_ecs_cluster" "main" {
