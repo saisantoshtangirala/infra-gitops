acfc043fdfd9bb8b5e77f14ee09a219c3c04ab8c
t.sai <t.sai@aligntech.com>
2025-04-19 23:57:00 +0530

    changed terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,25 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_ecs_cluster" "main" {
+  name = "web-app-cluster"
 }
 
-data "aws_secretsmanager_secret_version" "db" {
-  secret_id = "db_credentials"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
-  name    = "gitops-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["us-east-1a", "us-east-1b"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-# Sample ECS Service with secret env vars
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
-  name   = "web-app"
+resource "aws_ecs_task_definition" "web_task" {
+  family                   = "web-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = "256"
+  memory                  = "512"
+  execution_role_arn      = aws_iam_role.ecs_task_exec.arn
+  task_role_arn           = aws_iam_role.ecs_task_exec.arn
 
   container_definitions = jsonencode([
     {
       name      = "web"
       image     = "myrepo/web:latest"
-      cpu       = 256
-      memory    = 512
       essential = true
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 80
+          protocol      = "tcp"
         }
       ]
       environment = [
@@ -45,4 +34,39 @@ module "ecs" {
       ]
     }
   ])
+}
+
+resource "aws_ecs_service" "web_service" {
+  name            = "web-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.web_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = module.vpc.public_subnets
+    assign_public_ip = true
+    security_groups  = [aws_security_group.web_sg.id]
+  }
+}
+
+resource "aws_iam_role" "ecs_task_exec" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_attach" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
