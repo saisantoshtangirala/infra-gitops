f30e0c96407f886e93c991ebbdbf37a21fbad715
t.sai <t.sai@aligntech.com>
2025-04-20 00:44:03 +0530

    fixed vpc module compatability issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.5.0"
+  version = "4.0.2" # ✅ latest major version without classiclink issues
 
   name = "gitops-vpc"
   cidr = "10.0.0.0/16"
@@ -21,9 +21,20 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  create_igw = true
+
+  enable_nat_gateway     = false
+  single_nat_gateway     = false
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "gitops-vpc"
+  }
 }
+
 
 data "aws_secretsmanager_secret_version" "db" {
   secret_id = "db_credentials"
